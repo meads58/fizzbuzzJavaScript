4dfbbe46c221fd14637961113d72bde62dbbaafa
meads58 <meads109@gmail.com>
2015-03-03 13:49:02 +0000

    finished fizzbuzz in javascript

--- js/Javabuzz.js
+++ js/Javabuzz.js
@@ -9,8 +9,51 @@ var FizzBuzz = function() {};
     return this._isDivisible(number, 5);
   };
 
+  FizzBuzz.prototype.isDivisibleByFifteen = function(number) {
+    return this._isDivisible(number, 15);
+  };
+
   FizzBuzz.prototype._isDivisible = function(number, divisor) {
     return (number % divisor === 0);
   };
+
+  FizzBuzz.prototype.sayFizz = function(number) {
+    if (this.isDivisibleByThree(number)) {
+      return 'fizz'
+    };
+  };
+
+  FizzBuzz.prototype.sayBuzz = function(number) {
+    if (this.isDivisibleByFive(number)) {
+      return 'buzz'
+    };
+  };
+
+  FizzBuzz.prototype.sayFizzBuzz = function(number) {
+    if(this.isDivisibleByFifteen(number)) {
+      return 'fizzbuzz'
+    };
+
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -15,6 +15,10 @@ describe('FizzBuzz', function() {
       expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);
     });
 
+    it('divisible by fifteen', function() {
+      expect(fizzbuzz.isDivisibleByFifteen(15)).toBe(true);
+    });
+
   });
 
   describe('knows when a number is NOT', function() {
@@ -27,7 +31,50 @@ describe('FizzBuzz', function() {
     expect(fizzbuzz.isDivisibleByFive(1)).toBe(false);
    });
 
+   it('divisible by fifteen', function() {
+    expect(fizzbuzz.isDivisibleByFifteen(1)).toBe(false);
+   });
+
+  });
+
+
+  describe('know to say', function() {
+
+    it('fizz when divisible by three', function() {
+      expect(fizzbuzz.sayFizz(3)).toBe('fizz');
+    });
+
+    it('buzz when divisible by five', function() {
+      expect(fizzbuzz.sayBuzz(5)).toBe('buzz');
+    });
+
+    it('fizzbuzz when divisible by fifteen', function(){
+      expect(fizzbuzz.sayFizzBuzz(15)).toBe('fizzbuzz');
+    });
+
   });
 
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
